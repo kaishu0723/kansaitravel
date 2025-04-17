76aef4b01a02f132ad07467374f3e2f20f6972ed
kaishu <m.kaishu0723@gmailcom>
2025-04-17 15:57:59 +0900

    add db

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,9 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
 import { User } from "@/app/lib/usertype";
+import * as sqlite3 from 'sqlite3';
+import {open} from 'sqlite';
+import path from 'path';
+
+const db=await open({
+    filename:path.join(process.cwd(),'src/app/db/users.db'),
+    driver:sqlite3.Database
+});
 
 let users:User[]=[];
 
 export async function GET(){
+    const rows=await db.all('SELECT * FROM users');
+    console.log(rows);
+    users=rows;
     return NextResponse.json(users);
 }
 
@@ -12,8 +23,10 @@ export async function POST(req:NextRequest){
     if(!name){
         return NextResponse.json({error:'No name'},{status:400});
     }
-    const newUser={id:Date.now(),name};
+    const newId=Date.now();
+    const newUser={id:newId,name};
     users.push(newUser);
+    await db.run('INSERT INTO users (id,name) VALUES (?,?)',[newId,name]);
     return NextResponse.json(newUser,{status:201});
 }
 
@@ -23,5 +36,6 @@ export async function DELETE(req:NextRequest){
         return NextResponse.json({error:"No id"},{status:400});
     }
     users=users.filter((user)=>user.id!==id);
+    await db.run('DELETE FROM users WHERE id=?',id);
     return NextResponse.json({message:'delete'});
 }
